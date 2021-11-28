0131daef7752dcab50ec3041f101741760d824d1
fred <fredericnguyen777@gmail.com>
2021-11-28 17:34:42 -0500

    patched up before moving to rps-UI branch

--- game-script.js
+++ game-script.js
@@ -2,16 +2,19 @@
 // create constant for the computer's options. Allows for comparison to determine winner.
 const options = ['Rock', 'Paper', 'Scissors'];
 
+
 // Call the game() function in order to play 5 rounds.
 game()
+
 
 // Function that sets the computer's option for the round. 
 function computerPlay() {
   return options[Math.floor(Math.random() * 3 )]
 }
 
-// Function that calls computerPlay, plays one round of RPS, determining the winner or a tie. 
+// Function that calls computerPlay, plays one round of RPS.
 // Returns true if player wins, false if player loses and 0 if it is a tie.
+// Displays a message of the results of the round.
 function playRound(computerSelection, playerSelection) {
   if (playerSelection == options[0] && computerSelection == options[2] || playerSelection == options[1] && computerSelection == options[0] || playerSelection == options[2] && computerSelection == options[1]) {
     console.log("You Win! " + playerSelection + " Beats " + computerSelection)
@@ -41,17 +44,16 @@ function game() {
     // prompt for the player's selection.
     const playerSelection = capitalize(prompt("5 Round Match. Rock, Paper, Scissors?!"))
     // create a variable to store the result of the round. Call the playRound function.
-    results = playRound(computerPlay(),playerSelection); 
-    if (results) {
+    results = playRound(computerPlay(),playerSelection);
+
+    if (results) { //Tally wins and losses and determines the winner. 
       win++
     }
     else if (results === false) {
     lose++
     }
-    console.log(i)
-    console.log(win)
-    console.log(lose)
   }
+  // Display message of the results of the game.
   if (win > lose) {
     return console.log("Congratulations! You win!")
   }
